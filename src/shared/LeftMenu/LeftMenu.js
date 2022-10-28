3fa176a9c5e265793f2023bcded4a64f2c048bd4
Showrove Roy <showrove200@gmail.com>
2022-10-28 18:53:13 +0600

    get course subject from vercel api

--- src/shared/LeftMenu/LeftMenu.js
+++ src/shared/LeftMenu/LeftMenu.js
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
 
 const LeftMenu = () => {
+  // get & set course subject
+  const [subject, setSubject] = useState(null);
+
+  // get course subject
+  useEffect(() => {
+    fetch("https://next-knowledge-server.vercel.app/subject")
+      .then((res) => res.json())
+      .then((data) => setSubject(data))
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
     <div className='fixed'>
       <h3 className='text-2xl font-bold mt-5 mb-0 text-[#f71e7cdf]'>
