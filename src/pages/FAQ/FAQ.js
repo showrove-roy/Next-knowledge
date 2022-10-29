6ee4870d2b8036e77d544211ef2827eb2dd6b3a9
Showrove Roy <showrove200@gmail.com>
2022-10-29 22:28:45 +0600

    Home & faq page customize

--- src/pages/FAQ/FAQ.js
+++ src/pages/FAQ/FAQ.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const FAQ = () => {
-  return <div>This is faq page</div>;
+  return (
+    <div className='h-screen'>
+      <h1 className='text-3xl font-semibold text-center py-10'>
+        This is FAQ page
+      </h1>
+    </div>
+  );
 };
 
 export default FAQ;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,13 +4,22 @@ import React from "react";
 const Home = () => {
   return (
     <div>
+      <div>
+        <Player
+          autoplay
+          loop
+          src='https://assets2.lottiefiles.com/packages/lf20_cUG5w8.json'
+          className='h-24'
+          style={{ marginTop: "10px" }}></Player>
+        <h1></h1>
+      </div>
       <div className=''>
         <Player
           autoplay
           loop
           src='https://assets5.lottiefiles.com/packages/lf20_puciaact.json'
           className='md:h-[600px]'
-          style={{ marginTop: "-20px" }}></Player>
+          style={{ marginTop: "-40px" }}></Player>
       </div>
     </div>
   );
